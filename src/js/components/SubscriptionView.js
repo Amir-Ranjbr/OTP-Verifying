51e00bc88a9352a46658899948b482a359113de3
Amir Ranjbr <mr.am.amir.ir@gmail.com>
2024-03-26 16:02:00 +0330

    feat: add shake in input

--- src/js/components/SubscriptionView.js
+++ src/js/components/SubscriptionView.js
@@ -3,16 +3,16 @@
 export default function SubscriptionView() {
   return `
   <div class="container">
-  <span class="icon-box">
-  <i class="fas fa-shield-alt"></i>
-</span>
+      <span class="icon-box">
+        <i class="fas fa-shield-alt"></i>
+      </span>
       <h1>OTP Verification</h1>
       <p>Enter OTP Code</p>
       <div class="otp-input-container">
-        <input type="text" class="otp-input" maxlength="1" />
-        <input type="text" class="otp-input" maxlength="1" />
-        <input type="text" class="otp-input" maxlength="1" />
-        <input type="text" class="otp-input" maxlength="1" />
+        <input pattern="[0-9]" class="otp-input" maxlength="1" />
+        <input pattern="[0-9]" class="otp-input" maxlength="1" />
+        <input pattern="[0-9]" class="otp-input" maxlength="1" />
+        <input pattern="[0-9]" class="otp-input" maxlength="1" />
       </div>
       <span class="failed">Please enter code number</span>
       <button id="verify-btn" class="btn"><span>Verify OTP</span></button>
